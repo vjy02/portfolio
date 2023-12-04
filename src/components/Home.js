d418ccda42c7128ae308d0438f4f5603e66253f8
vjy02 <vyojoh@gmail.com>
2023-12-04 15:54:05 +1100

    Created Hero section

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import IconButton from '@mui/material/IconButton';
+
+export default function Home() {
+
+  return (
+    <div className="flex flex-col w-[85%] h-[60vh] md:h-[80vh] gap-4 md:gap-5 justify-center border border-red-300 ">
+      <div>
+        <h3 className="text-xl md:text-3xl mb-2">
+          Hello, I'm Victor.
+        </h3>
+        <h1 className="text-3xl md:text-5xl">
+          I'm a software engineer based in Melbourne, Victoria.
+        </h1>     
+      </div>
+
+      <div className="flex w-[40%] md:w-[10%] justify-between">
+        <IconButton aria-label="Github.com" onClick={() => window.open('https://github.com/vjy02')} style={{ color: 'black' }}>
+          <GitHubIcon />
+        </IconButton>
+
+        <IconButton aria-label="Linkedin.com" onClick={() => window.open('https://www.linkedin.com/in/victoryoshida02/')} style={{ color: 'black' }}>
+          <LinkedInIcon onClick={event =>  window.location.href='pagelink'} />
+        </IconButton>
+
+        <IconButton style={{ color: 'black' }}>
+          <EmailIcon disabled="true" />
+        </IconButton>
+      </div>
+
+    </div>
+  )
+}
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="flex items-center justify-center">
+      <Home />
+    </div>
   </React.StrictMode>
 );
