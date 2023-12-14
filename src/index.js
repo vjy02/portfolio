8e5e432066b00817480f59ce84c936c98a9c65d6
Victor Yoshida <Victor.Yoshida@cba.com.au>
2023-12-14 14:07:34 +1100

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar/>
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <Home />
     </div>
   </React.StrictMode>
