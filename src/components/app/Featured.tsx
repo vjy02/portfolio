d309deacc9a221a667b1f85760224ee401629968
vjy02 <vyojoh@gmail.com>
2024-02-28 18:34:57 +1100

    Added fade in, removed underline animation

--- src/components/app/Featured.tsx
+++ src/components/app/Featured.tsx
@@ -1,36 +1,31 @@
 "use client"
 
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { FeaturedCarousel } from "./custom-ui/FeaturedCarousel";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Featured(): JSX.Element {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
-    target: ref
+    target: ref,
+    offset: ["start end", "end start"],
   });
-  let scaleValue = useTransform(scrollYProgress, [0.2, 1], ['1', '0.1']);
+  let scaleValue = useTransform(scrollYProgress, [0, 0.7], ['0', '1']);
 
   return (
     <div id="featured" className="min-h-[55vh] lg:h-[100vh] flex justify-center items-center">
       <div className="flex flex-col items-center justify-between">
-        <section           ref={ref}>
-        <h1 
-          className="relative text-3xl md:text-5xl mb-5 md:mb-16 font-bold"
-        >
-          Featured Projects
-          <motion.div className="-z-10 absolute -bottom-3 left-0 right-0 h-1 bg-current" style={{ scaleX: scaleValue }}></motion.div>
-        </h1>
+        <section ref={ref}>
+          <motion.h1 
+            className="relative text-3xl md:text-5xl mb-5 md:mb-16 font-bold"
+            style={{ opacity: scaleValue }}
+          >
+            Featured Projects
+          </motion.h1>
         </section>
         
         <motion.div
-          whileInView={{ opacity: 1 }}
-          initial={{ opacity: 0 }}
-          viewport={{ once: true }}
-          transition={{
-            duration: 1,
-            delay: 0.75
-          }}
+          style={{ opacity: scaleValue}}
         >
           <FeaturedCarousel />
         </motion.div>
