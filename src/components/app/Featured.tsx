fb2647ce38acf9063488b3eb0b679d1d7ea45473
vjy02 <vyojoh@gmail.com>
2024-02-28 18:03:58 +1100

    Added underline scroll effect on title

--- src/components/app/Featured.tsx
+++ src/components/app/Featured.tsx
@@ -1,23 +1,28 @@
 "use client"
 
+import { useRef } from "react";
 import { FeaturedCarousel } from "./custom-ui/FeaturedCarousel";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Featured(): JSX.Element {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref
+  });
+  let scaleValue = useTransform(scrollYProgress, [0.2, 1], ['1', '0.1']);
+
   return (
     <div id="featured" className="min-h-[55vh] lg:h-[100vh] flex justify-center items-center">
       <div className="flex flex-col items-center justify-between">
-        <motion.h1 
-          whileInView={{ opacity: 1, y : 0 }}
-          initial={{  opacity: 0, y: 5 }}
-          viewport={{ once: true }}
-          transition={{
-            duration: 0.5,
-          }}
-          className="text-3xl md:text-5xl mb-5 md:mb-16 font-bold"
+        <section           ref={ref}>
+        <h1 
+          className="relative text-3xl md:text-5xl mb-5 md:mb-16 font-bold"
         >
           Featured Projects
-        </motion.h1>
+          <motion.div className="-z-10 absolute -bottom-3 left-0 right-0 h-1 bg-current" style={{ scaleX: scaleValue }}></motion.div>
+        </h1>
+        </section>
+        
         <motion.div
           whileInView={{ opacity: 1 }}
           initial={{ opacity: 0 }}
