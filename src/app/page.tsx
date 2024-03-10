fa8f0d6c4eb1e07de2282b11179d421fdf31bb06
vjy02 <vyojoh@gmail.com>
2024-03-10 17:36:43 +1100

    Fixed scroll progress not updating py

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,20 +47,20 @@ export default function Home() {
       <div className={`fixed flex top-0 right-[55px] px-0 py-[10px] h-full items-center justify-between ${isMobile ? "hidden" : ""}`}>
         <div className="block list-none m-0 p-0">
           <a
-            className={`block px-[2px] py-[9px] mx-0 my-[2px] ${theme === 'dark' ? 'bg-gray-500' : "bg-gray-200"} transition-all duration-300 ease-in-out 
-            ${heroInView ? `py-[19px] ${theme === 'dark' ? 'bg-white' : 'bg-slate-900'}` : ""}`}
+            className={`block px-[2px] mx-0 my-[2px] ${theme === 'dark' ? 'bg-gray-500' : "bg-gray-200"} transition-all duration-300 ease-in-out ${
+              heroInView ? `py-[19px] ${theme === 'dark' ? 'bg-white py-[20px]' : 'bg-gray-900 py-[20px]'}` : "py-[9px]"}`}
           >
             <span></span>
           </a>
           <a
-            className={`block px-[2px] py-[9px] mx-0 my-[2px] ${theme === 'dark' ? 'bg-gray-500' : "bg-gray-200"} transition-all duration-300 ease-in-out ${
-              featuredInView ? `py-[19px] ${theme === 'dark' ? 'bg-white' : 'bg-gray-900'}` : ""}`}
+            className={`block px-[2px] mx-0 my-[2px] ${theme === 'dark' ? 'bg-gray-500' : "bg-gray-200"} transition-all duration-300 ease-in-out ${
+              featuredInView ? `py-[19px] ${theme === 'dark' ? 'bg-white py-[20px]' : 'bg-gray-900 py-[20px]'}` : "py-[9px]"}`}
           >
             <span></span>
           </a>
           <a
-            className={`block px-[2px] py-[9px] mx-0 my-[2px] ${theme === 'dark' ? 'bg-gray-500' : "bg-gray-200"} transition-all duration-300 ease-in-out ${
-              contactMeInView ? `py-[19px] ${theme === 'dark' ? 'bg-white' : 'bg-gray-900'}` : ""}`}
+            className={`block px-[2px] mx-0 my-[2px] ${theme === 'dark' ? 'bg-gray-500' : "bg-gray-200"} transition-all duration-300 ease-in-out ${
+              contactMeInView ? `py-[19px] ${theme === 'dark' ? 'bg-white py-[20px]' : 'bg-gray-900 py-[20px]'}` : "py-[9px]"}`}
           >
             <span></span>
           </a>
