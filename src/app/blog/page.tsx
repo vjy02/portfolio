55cc2ef25bf7d1f5fbf4430743683cf78a21e1d9
vjy02 <vyojoh@gmail.com>
2025-02-17 22:01:35 +1100

    adjust blog page style and verify google search console

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,10 +2,10 @@ import BlogPostSummary from "@/components/app/components/BlogPostSummary";
 
 export default function Page() {
   return (
-    <section className="flex flex-col w-[80vw] md:w-[50vw] min-h-[35vh] md:h-fit mt-[10vh] items-center justify-center md:mx-auto">
+    <section className="flex flex-col w-[80vw] md:w-[50vw] min-h-[35vh] md:h-fit mt-[10vh] items-center justify-center mx-auto">
       <h1 className="relative text-3xl md:text-5xl mb-24 mt-10">
         hi, welcome to my blog.
-        <h4 className="absolute text-sm left-1/2 -translate-x-1/2 mt-4">still working on filling this page up!</h4>
+        <h4 className="absolute text-xs md:text-sm md:left-1/2 md:-translate-x-1/2 mt-4">still working on filling this page up!</h4>
       </h1>
       <BlogPostSummary />
     </section>
