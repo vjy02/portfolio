36c4f2bf6913581e1aef916b9b34229c7169944d
vjy02 <vyojoh@gmail.com>
2025-02-18 22:33:53 +1100

    adjust border hover color pt.4

--- src/components/app/components/BlogPostPill.tsx
+++ src/components/app/components/BlogPostPill.tsx
@@ -2,23 +2,17 @@
 
 import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 export default function BlogPostPill({ postData }: { postData: any }) {
-  const [isHover, setHover] = useState(false);
   const router = useRouter();
 
   return (
     <div
       className="w-full md:w-11/12 cursor-pointer flex"
-      onMouseOver={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
       onClick={() => router.push(`blog/${postData.slug}`)}
     >
       <div
-        className={`max-w-[75%] md:w-[80%] md:max-w-[80%] transition-all ease-in-out border-transparent border-l-4 pl-4 ${
-          isHover ? "border-0 border-solid translate-x-5 border-gray-400 dark:border-slate-300" : ""
-        }`}
+        className="max-w-[75%] md:w-[80%] md:max-w-[80%] transition-all ease-in-out border-transparent border-0 border-solid border-l-4 pl-4 hover:translate-x-5 hover:border-gray-400 hover:dark:border-slate-300"
       >
         <h3 className="font-bold text-sm md:text-xl">{postData.title}</h3>
         <p className="text-xs md:text-base">{postData.description}</p>
