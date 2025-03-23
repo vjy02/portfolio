8d95069420fc6be88696dafffe793a272d18d964
vjy02 <vyojoh@gmail.com>
2025-03-23 23:43:33 +1100

    adjust styling on mobile pt2

--- src/components/app/components/BlogPostPillLoading,.tsx
+++ src/components/app/components/BlogPostPillLoading,.tsx
@@ -1,11 +1,11 @@
 export default function BlogPostPillLoading() {
   return (
-    <div className="w-full md:10/12 cursor-pointer flex animate-pulse">
+    <div className="w-11/12 cursor-pointer flex animate-pulse">
       <div className="w-[80%] max-w-[80%] transition-all ease-in-out border-transparent border-0 border-solid border-l-4 pl-4">
         <div className="h-6 bg-gray-200 dark:bg-slate-700 rounded-md w-1/2 mb-2" />
         <div className="h-4 bg-gray-200 dark:bg-slate-700 rounded-md w-3/4" />
       </div>
-      <div className="ml-2 md:ml-auto h-4 bg-gray-200 dark:bg-slate-700 rounded-md w-12 md:w-16" />
+      <div className="ml-auto h-4 bg-gray-200 dark:bg-slate-700 rounded-md w-14" />
     </div>
   );
 }
