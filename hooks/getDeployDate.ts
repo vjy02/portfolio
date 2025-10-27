93e000c93db34e1152bedff8459755348bf0c44e
vjy02 <vyojoh@gmail.com>
2025-10-27 13:49:55 +1100

    update date format

--- hooks/getDeployDate.ts
+++ hooks/getDeployDate.ts
@@ -1,6 +1,10 @@
 export function getDeployDate(): string {
     const date = process.env.NEXT_PUBLIC_LAST_DEPLOYED_AT;
     return date
-        ? new Date(date).toLocaleString("en-US", { dateStyle: "long", timeStyle: "short" })
+        ? new Date(date).toLocaleDateString(undefined, {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        })
         : "Unknown";
 }
