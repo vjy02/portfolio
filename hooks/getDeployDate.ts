04e2a608c807e8b518219b25b599304a22683037
vjy02 <vyojoh@gmail.com>
2025-10-27 13:06:48 +1100

    test deploy date

--- hooks/getDeployDate.ts
+++ hooks/getDeployDate.ts
@@ -1,5 +1,5 @@
 export function getDeployDate(): string {
-    const date = process.env.VERCEL_BUILD_TIMESTAMP;
+    const date = process.env.NEXT_PUBLIC_LAST_DEPLOYED_AT;
     return date
         ? new Date(date).toLocaleString("en-US", { dateStyle: "long", timeStyle: "short" })
         : "Unknown";
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import fs from "fs";
+
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+
+const buildTime = new Date().toISOString();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_APP_VERSION: packageJson.version,
+    NEXT_PUBLIC_LAST_DEPLOYED_AT: buildTime,
+  },
 };
 
 export default nextConfig;
