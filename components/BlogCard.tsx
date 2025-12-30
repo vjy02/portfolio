fad12cd4af58249758d0682fa779bb1986832843
vjy02 <vyojoh@gmail.com>
2025-12-30 11:34:38 +1100

    edit spacing for blog card

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -13,11 +13,11 @@ export const BlogCard = ({
   <Link
     href={link}
     prefetch={true}
-    className="group flex items-center justify-between relative pr-4 transition-all ease-out border-transparent border-l-1 hover:pl-4 hover:border-l-3 hover:border-gray-600"
+    className="group flex items-center justify-between pr-1 relative transition-all ease-out border-transparent border-l-1 hover:pl-4 hover:border-l-3 hover:border-gray-600"
   >
     <div className="transition-opacity duration-300 group-hover:opacity-90 max-w-10/12">
       <h3 className="font-bold">{title}</h3>
-      <p className="text-xs text-gray-500 line-clamp-2 min-h-[2rem]">
+      <p className="text-xs text-gray-500 line-clamp-2 min-h-[2rem] mr-2">
         {description}
       </p>
     </div>
